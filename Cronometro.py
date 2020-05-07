98a66f2f1e9374531e13f3acce41679f0f2a30e8
Ariel <arefome@gmail.com>
2020-05-07 10:51:28 -0500

    correct and add files

--- Cronometro.py
+++ Cronometro.py
@@ -1,16 +1,17 @@
 import time
-
+import datetime
 
 class Cronometro:	
-
-	def __init__(self, hora, minuto, segundo, decima):
-		self._hora = int(hora)
-		self._minuto = int(minuto)
-		self._segundo = int(segundo)
-		self._decima = int(decima)
-
+	
+	def __init__(self):
+		self._hora = 0
+		self._minuto = 0
+		self._segundo = 0
+		self._decima = 0
+		self._activo = True
+		
 	def aumentoDecima(self):
-		time.sleep(.10)		
+		time.sleep(.001)		
 		self._decima +=1
 
 	def aumentoSegundo(self):		
@@ -50,21 +51,32 @@ class Cronometro:
 	def hora(self):
 		return self._hora
 	
-x = True	
-crono1 = Cronometro(0,0,50,8)
-while x:	
-		
-	crono1.aumentoDecima()
-	if(crono1.decima >= 10):
-		crono1.aumentoSegundo()
-		crono1.resetDecima()
-	elif (crono1.segundo >= 59):
-		crono1.aumentoMinuto()
-		crono1.resetSegundo()
-	elif(crono1.minuto >= 59):
-		crono1.aumentoHora()
-		crono1.resetMinuto()
-	elif(crono1.minuto == 1 and crono1.segundo == 2):
-		x = False
+	def iniciar(self):
+		if(self._activo):
+			self.aumentoDecima()
+			if(self.decima >= 99):
+				self.aumentoSegundo()
+				self.resetDecima()
+			elif (self.segundo >= 59):
+				self.aumentoMinuto()
+				self.resetSegundo()
+			elif(self.minuto >= 59):
+				self.aumentoHora()
+				self.resetMinuto()
+			
+	def mostrarTiempo(self):
+		tiempo = datetime.time(self.hora,self.minuto,
+							self.segundo, self.decima).strftime("%H:%M:%S.%f")
+		return tiempo
+				
+	def reiniciar(self):
+		self.resetDecima()
+		self.resetSegundo()
+		self.resetMinuto()
 	
-	print(crono1.hora,crono1.minuto,crono1.segundo,crono1.decima)
+		
+	def detener(self):
+		self._activo = False if self._activo else True
+			
+
+	
---
+++ VentanaCronometro.py
@@ -0,0 +1,58 @@
+import time
+import tkinter as tk
+from tkinter import Tk,Label,Button,Frame
+import datetime
+from Cronometro import Cronometro
+from tkinter.constants import BOTTOM
+
+
+class VentanaCronometro():
+    def __init__(self):
+        self.ventana = tk.Tk()
+        self.ventana.geometry('500x200')
+        self.ventana.resizable(0, 0)
+        self.ventana.title('Cronometro')
+        self.ventana.config(background = '#72a922')
+        self.cronometro_label = tk.Label(text="", font=('Tahoma', 44), fg='#ffffff', bg='#1f2f3f', pady=10, padx=10)
+        self.cronometro_label.place(x=20, y=30)
+        self.cronometro = Cronometro()
+        self.activo = True
+        self.frame = Frame(self.ventana)
+        self.cronometro_label.configure(text = self.cronometro.mostrarTiempo())
+        self.btnIniciar = Button(self.frame, fg='blue', text='Iniciar', command = self.actualizar)
+        self.btnIniciar.grid(row=1, column=1)
+        self.btnParar = Button(self.frame, fg='blue', text='Parar', command=self.parar)
+        self.btnParar.grid(row=1, column=2)
+        btnReiniciar = Button(self.frame, fg='blue', text='Reiniciar', command=self.reiniciar)
+        btnReiniciar.grid(row=1, column=3)
+        self.frame.pack(side = BOTTOM)
+        self.ventana.mainloop()
+        
+    def actualizar(self):
+        global proceso
+        self.cronometro.iniciar()
+        self.cronometro_label.configure(text = self.cronometro.mostrarTiempo())
+        proceso = self.ventana.after(10, self.actualizar)
+        self.btnIniciar.grid_forget()
+    
+    def parar(self):
+        global proceso
+        self.cronometro.detener()
+        self.ventana.after_cancel(proceso)
+        if self.activo:
+            self.btnParar.configure(text = 'Reanudar')
+            self.activo = False
+        else:
+            self.btnParar.configure(text = 'Parar')
+            self.activo = True
+            proceso = self.ventana.after(10, self.actualizar)
+    
+    def reiniciar(self):
+        global proceso
+        self.cronometro.reiniciar()
+        self.ventana.after_cancel(proceso)
+        self.cronometro_label.configure(text = self.cronometro.mostrarTiempo())
+        self.btnIniciar.grid(row=1,column=1)
+        self.frame.pack(side=BOTTOM)
+        
+main = VentanaCronometro()
